90d870d1ed591829a889ed6ebb7861e4e1b62911
dwjustin <dwjustin@gm.gist.ac.kr>
2022-07-31 16:44:21 +0900

    feat(App..js): added navigation

--- ours-app/App.js
+++ ours-app/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button, Pressable } from 'react-native';
 import React, {useState} from 'react';
-import { Button } from 'react-native-web';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   
@@ -23,7 +23,43 @@ export default function App() {
     setCount(count+1)
   }
 
+  const Stack=createStackNavigator();
+  function ScreenA({navigation}){
+    
+    const onPressHandler=()=>{
+      navigation.navigate('Screen_B')
+    }
+    
+    return(
+      <View>
+        <Pressable onPress={onPressHandler}>
+          Go to Screen B
+        </Pressable>
+      </View>
+    )
+  }
+  function ScreenB({navigation}){
+    
+    const onPressHandler=()=>{
+      navigation.navigate('Screen_A')
+    }
+    
+    return(
+      <View>
+        <Text>
+          Screen B
+        </Text>
+        <Pressable onPress={onPressHandler}>
+          Go back to Screen A
+        </Pressable>
+      </View>
+    )
+  }
+
+
   return (
+    
+    <NavigationContainer>
     <View style={styles.container}>
       <Text style={styles.text}>{name}</Text>
       <Text style={styles.text}>This is session number {session.number} and about</Text>
@@ -33,6 +69,12 @@ export default function App() {
       <Button title='Add' onPress={countHandler}></Button>
       <StatusBar style="auto" />
     </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Screen_A" component={ScreenA}/>
+      <Stack.Screen name='Screen_B' component={ScreenB}/>
+    </Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
 
@@ -42,6 +84,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 10,
+    margin: 40,
   },
   text: {
     color: '#ffffff',
